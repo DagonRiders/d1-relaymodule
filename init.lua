c19925ccd6b64ad6ffd9aeab5fdeeaf3d2266725
Aithne <MxAithne@gmail.com>
2018-01-01 18:53:24 +0000

    Turn relays off at start

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 dofile("config.lua")
 dofile("wificonnect.lua")
-dofile("redisrelaymodule.lua")
+dofile("relaymodule.lua")
+
+print("Turning all switches off")
+for i=0,7,1 do
+  gpio.mode(i+1, gpio.OUTPUT)
+  gpio.write(i+1, gpio.HIGH)
+end
+
 wificonnect()
